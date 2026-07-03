b4c0a37f07b3d5440309281b9eed46e5eac74a02
vicentematheus15 <vicentematheus1507@gmail.com>
2026-07-03 16:58:28 -0300

    Add pilha.py: implement stack data structure using LIFO principle

---
+++ Python/02 - Estrutura de dados/pilha.py
@@ -0,0 +1,9 @@
+# As pilhas sao listas do tipo LIFO (last in first out). as listas esse comportamento de maneira nativa em python
+pilha = []
+pilha.append(1)
+pilha.append(2)
+pilha.append(3)
+print(pilha)
+
+pilha.pop()
+print(pilha)
