3d932db0c6f0d839ea240dc6074d645d05937c99
vicentematheus15 <vicentematheus1507@gmail.com>
2026-07-03 17:43:16 -0300

    Add atividade_condicionais_loops.py: implement deque for fruit management and enhance list operations

--- Python/03 - Fluxos de controle/atividade_condicionais_loops.py
+++ Python/03 - Fluxos de controle/atividade_condicionais_loops.py
@@ -6,3 +6,25 @@ print(frutas)
 
 frutas.pop()
 print(frutas)
+
+from collections import deque
+
+frutas = deque()
+frutas.append("Banana")
+frutas.append("Ameixa")
+frutas.append("Laranja")
+frutas.append("Uva")
+frutas.append("Jabuticaba")
+print(frutas)
+
+frutas.popleft()
+for fruta in frutas:
+    print(fruta)
+
+
+for fruta in frutas:
+    if fruta == "Uva":
+        print(fruta)
+
+
+
