800fdcc4ee1e3592d590e2d35a034450159ab74e
vicentematheus15 <vicentematheus1507@gmail.com>
2026-07-03 17:13:03 -0300

    Add loops.py: implement for and while loops to iterate over a list and a range of numbers

---
+++ Python/03 - Fluxos de controle/loops.py
@@ -0,0 +1,12 @@
+#for: percorre toda a lista
+frutas = ['Goiaba', 'Laranja', 'Banana']
+print(frutas)
+
+for fruta in frutas:
+    print(fruta)
+
+#while: repete até alguma condição ser atingida
+num = 1
+while num <= 5:
+    print(num)
+    num +=1
