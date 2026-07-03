af24af29ef5b99a2e5e133422d64e53cba77c561
vicentematheus15 <vicentematheus1507@gmail.com>
2026-07-03 16:06:00 -0300

    Add pacotes.py: implement module and function imports from meu_pacote

---
+++ Python/02 - Estrutura de dados/pacotes.py
@@ -0,0 +1,7 @@
+# PACOTE > MODULOS > FUNCOES
+
+#importando um modulo específico de um pacote
+import meu_pacote.meu_modulo
+
+#importando funcoes específicas de um módulo, que pertence a um pacote
+from meu_pacote.meu_modulo import minha_funcao
